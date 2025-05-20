4dcf956129626f5a9125ba6ae098a951ab6d0a9d
naascimento0 <gabrieloliveiran@hotmail.com>
2025-05-19 21:36:27 -0300

    spring-boot java files

---
+++ src/main/java/nemo/seon/SeonBackendApplication.java
@@ -0,0 +1,11 @@
+package nemo.seon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeonBackendApplication.class, args);
+    }
+}
